0ddcb96c33a1ba5ea3ac131f716ad18ef20b4868
Victoria Moore <victoriamoore563@gmail.com>
2020-03-10 21:38:44 -0700

    new function added

--- recursion.js
+++ recursion.js
@@ -14,3 +14,97 @@ function countSheep(totalSheep) {
 
 countSheep(3)
 
+// What is the input to the program? -- 
+// What is the output of the program? -- 
+// What is the input to each recursive call? -- 
+// What is the output of each recursive call? -- 
+
+function powerCalculator() {
+
+}
+
+// What is the input to the program? -- the string  
+// What is the output of the program? -- the string in reverse
+// What is the input to each recursive call? -- str minus the character
+// What is the output of each recursive call? -- reversed string
+
+function reverseString(str) {
+    if (str === '') {
+        return '';
+    } 
+    console.log(str)
+    reverseString(str.substr(1)) + str.charAt(0)
+}
+
+reverseString('hello')
+
+
+// What is the input to the program? -- 
+// What is the output of the program? -- 
+// What is the input to each recursive call? -- 
+// What is the output of each recursive call? -- 
+
+function triangleNumber(n) {
+    if (n <= 1) {
+        return;
+    }
+    console.log(n)
+    triangleNumber(n * (n + 1) / 2);
+}
+
+triangleNumber(6)
+
+// What is the input to the program? -- 
+// What is the output of the program? -- 
+// What is the input to each recursive call? -- 
+// What is the output of each recursive call? -- 
+
+
+
+// What is the input to the program? -- 
+// What is the output of the program? -- 
+// What is the input to each recursive call? -- 
+// What is the output of each recursive call? -- 
+
+
+
+// What is the input to the program? -- 
+// What is the output of the program? -- 
+// What is the input to each recursive call? -- 
+// What is the output of each recursive call? -- 
+
+
+
+
+// What is the input to the program? -- 
+// What is the output of the program? -- 
+// What is the input to each recursive call? -- 
+// What is the output of each recursive call? -- 
+
+
+
+// What is the input to the program? -- 
+// What is the output of the program? -- 
+// What is the input to each recursive call? -- 
+// What is the output of each recursive call? -- 
+
+
+
+// What is the input to the program? -- 
+// What is the output of the program? -- 
+// What is the input to each recursive call? -- 
+// What is the output of each recursive call? -- 
+
+
+
+// What is the input to the program? -- 
+// What is the output of the program? -- 
+// What is the input to each recursive call? -- 
+// What is the output of each recursive call? -- 
+
+
+
+// What is the input to the program? -- 
+// What is the output of the program? -- 
+// What is the input to each recursive call? -- 
+// What is the output of each recursive call? -- 
